81652140340283331e8d3c99bf1e2e1c6cb82a9b
Vanya Prokopovich <iprokopovich@circadianrisk.com>
2020-04-16 20:00:32 -0400

    got the cartogram to work

--- d3-playground/vanyas-tutorials/d3-cartogram/cartogram.js
+++ d3-playground/vanyas-tutorials/d3-cartogram/cartogram.js
@@ -7,30 +7,26 @@ let nation
 us.then((data) => {
     stateBoundaries = topojson.mesh(data, data.objects.states, (a, b) => a !== b)
     nation = topojson.mesh(data, data.objects.nation)
+
+    const svg = d3.select('#d3-cartogram')
+        .attr("viewBox", `0 0 960 ${height}`)
+        .style("width", "100%")
+        .style("height", "auto")
+
+    svg.append("path")
+        .classed("state-boundaries", true)
+        .datum(stateBoundaries)
+        .attr("fill", "none")
+        .attr("stroke", "lightgray")
+        .attr("stroke-width", 1)
+        .attr("stroke-linejoin", "round")
+        .attr("d", d3.geoPath());
+
+    svg.append("path")
+        .classed("nation-boundary", true)
+        .datum(nation)
+        .attr("fill", "none")
+        .attr("stroke", "gray")
+        .attr("stroke-linejoin", "round")
+        .attr("d", d3.geoPath());
 })
-
-console.log('geoPath', geoPath)
-console.log(nation, stateBoundaries)
-
-
-const svg = d3.select('#d3-cartogram')
-    .attr("viewBox", `0 0 960 ${height}`)
-    .style("width", "100%")
-    .style("height", "auto")
-
-svg.append("path")
-    .classed("state-boundaries", true)
-    .datum(stateBoundaries)
-    .attr("fill", "none")
-    .attr("stroke", "lightgray")
-    .attr("stroke-width", 1)
-    .attr("stroke-linejoin", "round")
-    .attr("d", geoPath);
-
-svg.append("path")
-    .classed("nation-boundary", true)
-    .datum(nation)
-    .attr("fill", "none")
-    .attr("stroke", "gray")
-    .attr("stroke-linejoin", "round")
-    .attr("d", geoPath);
